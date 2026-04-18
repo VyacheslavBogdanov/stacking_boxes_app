7418788c10160215513a1d52ee2093ef1b934c9e
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2026-04-18 17:24:08 +0400

    init project

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
+export default defineConfig({
+	plugins: [vue(), vueDevTools()],
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://81.94.156.176:5011',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
+	},
+});
